842462761340d6088d01f66faf6ec96548aab119
clairekan <40970135+clairekan@users.noreply.github.com>
2018-11-25 23:47:38 +0800

    1

--- HW7/first.R
+++ HW7/first.R
@@ -23,7 +23,7 @@ Sim_CI<-function(n,mu,sd,m,conf.level){
     CI.half[i]<-t.value*se.x[i]
     CI.Up[i]<-mean.x[i]+CI.half[i]
     CI.Lower[i]<-mean.x[i]-CI.half[i]
-    if(CI.Up[i]>mu & CI.Lower<mu) Check[i]<-1 else Check[i]<-0
+    if(CI.Up[i]>mu & CI.Lower[i]<mu) Check[i]<-1 else Check[i]<-0
   }
   
   plot(c(CI.Up,CI.Lower),type="n",pch=19,xlim=c(1,m),xlab="Trial",ylab=expression(mu))  #n=甚麼都不畫
@@ -63,6 +63,35 @@ Sim_CI<-function(n,mu,sd,m,conf.level){
 
 Sim_CI(n=30, m=100, mu=50, sd=5, conf.level=0.95)
   
+#t-distribution with df=30
+curve(dt(x,df=30),from=-3,to=3,lwd=4,ylab="y")
+#t-distribution function
+qt(0.01,df=10,lower.tail=F)#求t*
+
+dt(x,f=2)
+
+pt(2,df=10,lower.tail=F)#用t求mean
+
+
+####11.32 example####
+
+plot(c(0,3),type='n',xlim=c(6,9),xlab='Mean hour of sleep',ylab='',axes=F,
+     main='Comparsion of the two confidence interval')
+
+lines(c(7.1,8.22),c(2,2))
+text(6.9,2,labels='Stat10')
+lines(c(7.1,7.1),c(1.95,2.05))
+lines(c(7.66,7.66),c(1.95,2.05))
+lines(c(8.22,8.22),c(1.95,2.05))
+
+lines(c(6.53,7.09),c(1,1))
+text(6.3,1,labels='Stat13')
+lines(c(6.53,6.53),c(0.95,1.05))
+lines(c(6.81,6.81),c(0.95,1.05))
+lines(c(7.09,7.09),c(0.95,1.05))
+
+axis(1)
+
 
 
 
